a2a2e63a6b2f00f6547d170cffad32ea947ec2c9
slimeVidas26 <dahan.isaac@gmail.com>
2018-10-31 19:02:25 +0200

    two-way-binding

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
-import Users from './users/Users';
 
 
 
 class App extends Component {
+  state = {
+    name : "Techsith"
+  }
+
+  changeName= (newName) =>{
+    this.setState({
+        name : newName
+    });
+  }
+  changeNameFromInput= (event) =>{
+    this.setState({
+        name : event.target.value
+    });
+  }
+
   render() {
     return (
       <div className="App">
-       <Users title = "Users List"/>
+        <br/>  <br/>
+        <button onClick = {()=> this.changeName('Awesome Techsith anonymous')}>Change state using anonymous function</button>
+        <button onClick = { this.changeName.bind(this,'Awesome Techsith binding')}>Change state using binding function</button>
+
+        <br/>  <br/>
+        <input type="text" onChange = {this.changeNameFromInput} value = {this.state.name}/>
+        <div>{this.state.name}</div>
+
+     
       </div>
     );
   }
