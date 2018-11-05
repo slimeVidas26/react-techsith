53f0a9730f462107c70eb6324f7ec61bd6ce0218
slimeVidas26 <dahan.isaac@gmail.com>
2018-11-05 22:30:49 +0200

    list and key again

--- src/users/User.js
+++ src/users/User.js
@@ -1,23 +1,15 @@
 import React from 'react';
 
-const User = (props)=>{
-    let age = props.age?props.age:'NA';
-    if(props.children){
-        return(
-            <li>
-               <span>Name : {props.children}  |  age : {age} </span> 
-               <input onChange={props.changeEvent}  value={props.children}/>
-               <button onClick={props.delete} className ="btn btn-primary">Delete</button>
-            </li>
-        )
-    }else{
-        return(
-            <div>
-              Invalid User
-            </div>
-        )
-    }
-    
+const User  = (props)=>{
+    return (
+    <li>
+        My name is {props.children} and i'm {props.age} years old
+        <input type="text" onChange = {props.changeEvent} value = {props.children}/>
+        <button onClick={props.delEvent}
+         className = "btn btn-primary">Delete {props.children}
+         </button>
+   </li>
+    )
 }
 
 export default User;
